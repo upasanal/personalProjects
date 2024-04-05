a66f76b98b92dc2b9f538d3b6e5b6214927d8a0d
upasanal <111466862+upasanal@users.noreply.github.com>
2024-04-04 22:08:39 -0400

    trying

---
+++ coStar/coStar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coStar
+//
+//  Created by Upasana Lamsal on 4/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coStar/coStar/coStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  coStarApp.swift
+//  coStar
+//
+//  Created by Upasana Lamsal on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct coStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
